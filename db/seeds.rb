993924b727a3e28935e2fd56c192df40234db674
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:51:55 -0500

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Non-admin User creation
+User.create(
+  username: "Iggy95",
+  password: "password",
+  name: "Igor Veyner",
+  birthdate: "1995-02-10",
+  funds: 1000000
+)
