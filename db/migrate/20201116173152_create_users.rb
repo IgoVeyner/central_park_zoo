f36d5122d3d9b67b48b7d612bf89753aa6fa0b97
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:36:00 -0500

    update user migration

--- db/migrate/20201116173152_create_users.rb
+++ db/migrate/20201116173152_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      
+      t.string :username
+      t.string :password_digest
+      t.string :name
+      t.datetime :birthdate
+      t.integer :funds
+      t.boolean :admin
       t.timestamps
     end
   end
