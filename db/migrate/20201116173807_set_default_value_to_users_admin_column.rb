0da6a0ed827affbe3586e84bf2b4abe84dc6f04f
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:40:50 -0500

    crate migration of admin column default

---
+++ db/migrate/20201116173807_set_default_value_to_users_admin_column.rb
@@ -0,0 +1,9 @@
+class SetDefaultValueToUsersAdminColumn < ActiveRecord::Migration[5.2]
+  def up
+    change_column :users, :admin, :boolean, default: false
+  end
+
+  def down
+    change_column :users, :admin, :boolean, default: nil
+  end
+end
