85a461407ec0644f1169ee579f29a906ab14edbc
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:45:37 -0500

    add notes for future user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # Todo: Validations
+  # Username: presence, uniqueness
+  # Name: No numbers or symbols
+  # Birthdate: Not after today's date
+  # Funds: cannot be negative
 end
