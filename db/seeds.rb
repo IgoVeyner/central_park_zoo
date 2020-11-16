d2ff3792279406a7b8d3f36bb1e707999465d47a
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:56:18 -0500

    add admin creation to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@ User.create(
   birthdate: "1995-02-10",
   funds: 1000000
 )
+
+# Admin User creation
+User.create(
+  username: "sIrwin",
+  password: "password",
+  name: "Steve Irwin",
+  birthdate: "1962-02-22",
+  funds: 10000000,
+  admin: true
+)
