ae11517964408be5612972757ace2ef1005bd03d
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 15:38:14 -0500

    add route to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#home'
+  
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create', as: 'create_user'
 
