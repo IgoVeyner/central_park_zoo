820152a1a5cd31f9138f8ecaa5fedd26ba246598
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 14:10:26 -0500

    add username validation to test error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   # Todo: Validations
-  # Username: presence, uniqueness
   # Name: No numbers or symbols
   # Birthdate: Not after today's date
   # Funds: cannot be negative
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
