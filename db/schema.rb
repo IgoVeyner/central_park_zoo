a4caaf3433ad193e5088a12a789b127b9eb1eaac
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 12:41:10 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_173152) do
+ActiveRecord::Schema.define(version: 2020_11_16_173807) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_173152) do
     t.string "name"
     t.datetime "birthdate"
     t.integer "funds"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
