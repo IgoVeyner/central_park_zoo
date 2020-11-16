33c0e43ebfb6779a0de69f32a58ce58e2b66a347
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 15:48:37 -0500

    add private authenticated method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   private
 
+  def authenticated
+    !!current_user
+  end
+
   def current_user
     User.find_by_id(session[:user_id])
   end
