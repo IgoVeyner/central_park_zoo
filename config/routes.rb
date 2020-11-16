215db0767f2bb62c020d2658ce4d3109e38b29ca
Igor Veyner <igorveyner95@gmail.com>
2020-11-16 15:00:25 -0500

    add route for user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create', as: 'create_user'
+
+  get '/guest/:id', to: 'users#show', as: 'user'
 end
