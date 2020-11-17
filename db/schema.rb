80adc7de1b9cb240e7f3c9d8e011eecb504f3af2
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 16:48:02 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_203331) do
+ActiveRecord::Schema.define(version: 2020_11_17_214457) do
 
   create_table "exhibits", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2020_11_17_203331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
+    t.string "common_name"
+    t.string "conservation_status"
+    t.string "image_url"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
