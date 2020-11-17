71743745d89e4e261eb49a85065a5bd832f7548e
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 15:51:32 -0500

    add notes for future exhibit validations

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -1,4 +1,8 @@
 class Exhibit < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  # Todo validations
+  # Biome no non-letters
+  # Description max length 500
 end
