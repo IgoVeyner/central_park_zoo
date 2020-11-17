f515c3c14e3615d0ab9262954bcc4d1e9f04da33
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 09:14:49 -0500

    add function to session create

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,15 @@ class SessionController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by_id(params[:user_name])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      # add flash error message
+      @users = User.all
+      render :new
+    end
   end
 end
