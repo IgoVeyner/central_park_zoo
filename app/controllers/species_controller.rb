fca4542781fa6481754c6fd8a30efc9bd1e44b7d
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 17:28:13 -0500

    add private method to find species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -3,4 +3,11 @@ class SpeciesController < ApplicationController
   def index
     @species = Species.all
   end
+
+  private 
+
+  def find_species
+    @species = Species.find_by_id(params[:id])
+    redirect_to_errors_page(Species.name) unless @species
+  end
 end
