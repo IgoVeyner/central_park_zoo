575e1e5ab6fa99964c111638d73f85dc426c2639
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 13:17:50 -0500

    user's name and default funds value set on github signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(:username => auth_hash["info"]["email"]).first_or_create do |user|
       user.password = SecureRandom.hex
+      user.name = auth_hash["info"]["name"]
+      user.funds = 100
     end
   end
 end
