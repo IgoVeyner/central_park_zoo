b56d67797223cb3286cfe4868bab53de0618e4c1
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 15:39:33 -0500

    add pirvate method to find exhibit

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -2,4 +2,10 @@ class ExhibitsController < ApplicationController
   def index
     @exhibits = Exhibit.all
   end
+
+  private 
+
+  def find_exhibit
+    @exhibit = Exhibit.find_by_id[params[:id]]
+  end 
 end
