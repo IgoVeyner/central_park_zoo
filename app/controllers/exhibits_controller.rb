ab3e3fa0e4e3271dd7b544f814608c45b4461a18
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 15:50:29 -0500

    add before action to redirect anon users

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,5 +1,6 @@
 class ExhibitsController < ApplicationController
   before_action :find_exhibit, only: [:show]
+  before_action :redirect_anon_users_to_home
 
   def index
     @exhibits = Exhibit.all
