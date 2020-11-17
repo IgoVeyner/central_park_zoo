aa0e46f2ad8c93f0967b3ad75eab6276b0420093
Igor Veyner <igorveyner95@gmail.com>
2020-11-17 17:02:02 -0500

    add species creation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,12 @@ Exhibit.create(
   image_url: "https://www.cooperation.com.au/Photo%20Albums/Canada-NYC%202006/10%20-%20New%20York%20City/slides/Sea%20Lion%20Central%20Park%20Zoo.JPG",
   description: "At the heart of the park, our sea lion gang can be seen sunning themselves or swimming amidst the Manhattan skyline. The exhibit features both above and below water viewing of some of our most acrobat swimmers."
 )
+
+# Species creation
+Species.create(
+  name: "Zalophus californianus",
+  common_name: "California Sea Lion",
+  conservation_status: "Least Concern",
+  image_url: "https://en.wikipedia.org/wiki/California_sea_lion#/media/File:California_sea_lion_in_La_Jolla_(70568).jpg",
+  description: "The California sea lion (Zalophus californianus) is a coastal eared seal native to western North America. It is one of six species of sea lion. Its natural habitat ranges from southeast Alaska to central Mexico, including the Gulf of California. They mainly haul-out on sandy or rocky beaches, but they also frequent manmade environments such as marinas and wharves. Sea lions feed on a number of species of fish and squid, and are preyed on by killer whales and great white sharks."
+)
