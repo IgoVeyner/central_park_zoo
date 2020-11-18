acdb7ed5567363b421bb6c00fb7a8b5b24c084f9
Igor Veyner <igorveyner95@gmail.com>
2020-11-18 16:06:20 -0500

    fix typo in name of a migration

--- db/migrate/20201117215633_set_default_value_to_species_image_url_column.rb
+++ db/migrate/20201117215633_set_default_value_to_species_image_url_column.rb
@@ -1,4 +1,4 @@
-class SetDefaultValueToSealsImageUrlColumn < ActiveRecord::Migration[5.2]
+class SetDefaultValueToSpeciesImageUrlColumn < ActiveRecord::Migration[5.2]
   def up
     change_column :species, :image_url, :string, default: "https://www.drupal.org/files/issues/2019-07-21/missing.png"
   end
