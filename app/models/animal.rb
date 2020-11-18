174887172c41faeba23de5575294307cdb97fbcf
Igor Veyner <igorveyner95@gmail.com>
2020-11-18 18:15:41 -0500

    add notes for future animal validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,4 +3,10 @@ class Animal < ApplicationRecord
   belongs_to :exhibit
 
   validates :name, presence: true
+
+  # Todo Validations:
+  # age no non numbers, presence
+  # weight no non numbers, presence
+  # description length 500
+  # exhibit_id & species_id  presence
 end
