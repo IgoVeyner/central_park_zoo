4514bb8a8747a5c9966cf161e712a0457427bb35
Igor Veyner <igorveyner95@gmail.com>
2020-11-18 15:32:22 -0500

    add notes for later species validations

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,9 @@
 class Species < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  # Todo validations
+  # common_name no non letters
+  # conservation status presence
+  # description length 500
 end
