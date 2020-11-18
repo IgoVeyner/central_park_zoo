e69ff1238a8257fb64bed688549329e7e6ba692c
Igor Veyner <igorveyner95@gmail.com>
2020-11-18 16:13:07 -0500

    add nested routes for animals

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   
   resources :exhibits, only: [:index, :show] do 
     resources :species, only: [:index]
+    resources :animals, only: [:index, :show]
   end
 
   resources :species, only: [:index, :show]
