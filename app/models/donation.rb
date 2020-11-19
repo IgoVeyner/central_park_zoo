bfbf100ac5ad743e8f9ddd8d38f7ef4a4d697f02
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 12:20:22 -0500

    add to donation_total when donating

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -11,7 +11,9 @@ class Donation < ApplicationRecord
   def add_donation
     if user.funds >= amount
       user.funds -= amount
+      species.donation_total += amount
       user.save
+      species.save
       "Thanks for donating $#{amount} to #{species.name}"
     else
       "Sorry, you do not have enough funds to donate. Your funds: $#{user.funds}"
