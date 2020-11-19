0d5b6cb0189e6f47574427861d91d54d9db13d5c
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 10:29:09 -0500

    save user after editing their funds

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,11 +2,10 @@ class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :species
 
-  private
-
   def add_donation
     if user.funds >= amount
       user.funds -= amount
+      user.save
       "Thanks for donating $#{amount} to #{species.name}"
     else
       "Sorry, you do not have enough funds to donate. Your funds: #{user.funds}"
