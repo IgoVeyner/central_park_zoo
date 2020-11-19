abdae742a8720ceae32fe7d0a897803ab28b2b96
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 10:22:37 -0500

    give donation model private method to allow users to donate

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,16 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :species
+
+  private
+
+  def add_donation
+    if user.funds >= amount
+      user.funds -= amount
+      "Thanks for donating $#{amount} to #{species.name}"
+    else
+      "Sorry, you do not have enough funds to donate. Your funds: #{user.funds}"
+    end
+  end
+  
 end
