a8a757a0cc9013e39becb40d9697222bb205e6df
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 16:20:13 -0500

    fix most_donation not rendering

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   
   match '/auth/:github/callback', to: 'session#github_login', via: [:get, :post]
 
+  get 'guests/most_donations', to: 'users#most_donations', as: 'most_donation'
   resources :guests, controller: 'users', as: 'user' do
     resources :donations, only: [:index]
-    get '/most_donations', to: 'users#most_donations', as: 'most_donation'
   end
 
   resources :exhibits, only: [:index, :show] do 
