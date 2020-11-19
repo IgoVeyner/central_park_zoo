288e02ee10cdee605e52035c132e089ff2d001f9
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 17:47:37 -0500

    teach species to find its top donor

--- app/models/species.rb
+++ app/models/species.rb
@@ -11,4 +11,10 @@ class Species < ApplicationRecord
   # common_name no non letters
   # conservation status presence
   # description length 500
+
+  def top_donor
+    donation_totals_by_user = donations.group(:user_id).sum(:amount)
+    user_id = donation_totals_by_user.key(donation_totals_by_user.values.max)
+    User.find_by_id(user_id)
+  end
 end
