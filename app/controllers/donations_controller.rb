7ec08f5d0b3155286bb29607606671a67bcdb595
Igor Veyner <igorveyner95@gmail.com>
2020-11-19 10:07:24 -0500

    add private method donation_params

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,4 +6,10 @@ class DonationsController < ApplicationController
   def create
     raise "donations#create".inspect
   end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:amount, :comment, :species_id)
+  end
 end
