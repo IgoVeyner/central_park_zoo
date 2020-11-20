b9340cec74beb5a3d414d8a8fd130502dfb613aa
Igor Veyner <igorveyner95@gmail.com>
2020-11-20 13:16:28 -0500

    add has_access users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def average_donation_amount(user)
     user.average_donation
   end
+
+  def has_access
+    User.find_by_id(session[:user_id]) == User.find_by_id(params[:id])
+  end
 end
