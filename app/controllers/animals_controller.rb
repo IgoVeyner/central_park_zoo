255aedd5904004c137972606a471cda9c5f137ba
Igor Veyner <igorveyner95@gmail.com>
2020-11-20 17:20:16 -0500

    change index action to work with unnested route

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,13 +20,17 @@ class AnimalsController < ApplicationController
   end
 
   def index
-    @exhibit = Exhibit.find_by_id(params[:exhibit_id])
+    if params[:exhibit_id]
+      @exhibit = Exhibit.find_by_id(params[:exhibit_id])
 
-    if @exhibit 
-      @animals = @exhibit.animals
-    else 
-      flash[:message] = "#{params[:exhibit_id]} is not a valid Exhibit"
-      render "partials/error"
+      if @exhibit 
+        @animals = @exhibit.animals
+      else 
+        flash[:message] = "#{params[:exhibit_id]} is not a valid Exhibit"
+        render "partials/error"
+      end
+    else
+      @animals = Animal.all
     end
   end
 
