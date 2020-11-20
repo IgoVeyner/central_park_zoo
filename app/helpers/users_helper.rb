8092f454256188fc1c2a85787e8cc161d2cbcbec
Igor Veyner <igorveyner95@gmail.com>
2020-11-20 13:35:05 -0500

    add helper to only allow you to see your own donations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
   def has_access
     User.find_by_id(session[:user_id]) == User.find_by_id(params[:id])
   end
+
+  def has_donation_access
+    User.find_by_id(session[:user_id]) == User.find_by_id(params[:user_id])
+  end
 end
