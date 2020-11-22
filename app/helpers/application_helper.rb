2f96e78d825d84cd528208db0a10f61b30f6c8ef
Igor Veyner <igorveyner95@gmail.com>
2020-11-22 13:50:11 -0500

    move render error to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def is_admin
     current_user.admin
   end
+
+  def render_error(id, class_name, target)
+    flash[:message] = "#{id} is not a valid #{class_name}"
+    redirect_to target
+  end
 end
