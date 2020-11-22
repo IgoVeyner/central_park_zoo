06447130e46f3533710d1538445db70fe50f9340
Igor Veyner <igorveyner95@gmail.com>
2020-11-22 13:56:27 -0500

    move helper back to controller, redirect does not work outside of it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,4 @@ module ApplicationHelper
   def is_admin
     current_user.admin
   end
-
-  def render_error(id, class_name, target)
-    flash[:message] = "#{id} is not a valid #{class_name}"
-    redirect_to target
-  end
 end
