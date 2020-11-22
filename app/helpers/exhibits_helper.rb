ec022fe30eb6cebf5dc2a2666ee7f97ce418dd88
Igor Veyner <igorveyner95@gmail.com>
2020-11-22 15:15:32 -0500

    remove find exhibit helper

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -1,10 +1,2 @@
 module ExhibitsHelper
-
-  def find_exhibit_or_redirect
-    id = params[:exhibit_id] || params[:id]
-    @exhibit = Exhibit.find_by_id(id) 
-    render_error(id, Exhibit.name, exhibits_path) unless @exhibit
-    @exhibit
-  end 
-
 end
