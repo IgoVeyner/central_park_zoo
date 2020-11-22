66ceafc259a9ecc017097b23de8f3a3da9f0a7c2
Igor Veyner <igorveyner95@gmail.com>
2020-11-22 15:26:10 -0500

    update to use render_error method

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -43,7 +43,7 @@ class SpeciesController < ApplicationController
 
   def find_species
     @species = Species.find_by_id(params[:id])
-    redirect_to_errors_page(Species.name) unless @species
+    render_error(params[:id], Species.name, species_index_path) unless @species
   end
 
   def find_exhibit
