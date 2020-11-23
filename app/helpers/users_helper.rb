30a35587373e03a14e26fc19e75a16530329cfa1
Igor Veyner <igorveyner95@gmail.com>
2020-11-23 14:03:10 -0500

    refactor has_access to use current_user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,6 @@ module UsersHelper
 
   def has_access
     id = params[:user_id] || params[:id]
-    User.find_by_id(session[:user_id]) == User.find_by_id(id)
+    current_user == User.find_by_id(id)
   end
 end
