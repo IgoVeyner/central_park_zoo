782005f770dcacf1743221d3541b79d8ea728eae
Igor Veyner <igorveyner95@gmail.com>
2020-11-29 13:22:36 -0500

    update seal exhibit image

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ User.create(
 seal_pool = Exhibit.create(
   name: "Central Garden & Sea Lion Pool",
   biome: "Pacific Ocean coastline",
-  image_url: "https://www.cooperation.com.au/Photo%20Albums/Canada-NYC%202006/10%20-%20New%20York%20City/slides/Sea%20Lion%20Central%20Park%20Zoo.JPG",
+  image_url: "https://i.pinimg.com/originals/21/aa/ab/21aaab13a04b7e18d80a1fef19f268f0.jpg",
   description: "At the heart of the park, our sea lion gang can be seen sunning themselves or swimming amidst the Manhattan skyline. The exhibit features both above and below water viewing of some of our most acrobat swimmers."
 )
 
